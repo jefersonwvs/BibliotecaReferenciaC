551aa6eb343919069b5a9405ed73cd9117bd1f1f
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2021-02-24 09:46:56 -0400

    BST source code has been translated into English

--- Tree/BinarySearchTree.c
+++ Tree/BinarySearchTree.c
@@ -2,306 +2,313 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct vertice {
-    struct vertice* esq;
-    int chave;
-    struct vertice* dir;
+typedef struct vertex* Vertex;
+struct vertex {
+   Vertex left;
+   int key;
+   Vertex right;
 };
-typedef struct vertice Vertice;
-typedef struct vertice* ArvBinPesq;    // Árvore Binária de Pesquisa;
 
-int arvoreVazia(ArvBinPesq*);
-ArvBinPesq criaVertice(int);
-int insereVertice(ArvBinPesq*, int);
-int removeVertice(ArvBinPesq*, int);
-ArvBinPesq antecessorVertice(ArvBinPesq*);
-ArvBinPesq sucessorVertice(ArvBinPesq*);
-int buscaVertice(ArvBinPesq*, int);
-void preOrdem(ArvBinPesq);
-void emOrdem(ArvBinPesq);
-void posOrdem(ArvBinPesq);
-void representacaoAninhada(ArvBinPesq);
-void representacaoIndentada(ArvBinPesq, int);
-void constroiArvore(ArvBinPesq*, char*);
-void concatenarCaractere(char*, char);
-void destroiArvore(ArvBinPesq*);
-int giraVertice(ArvBinPesq*, int, int);
-int rotacaoEsquerda(ArvBinPesq*);
-int rotacaoDireita(ArvBinPesq*);
+int isEmpty(Vertex);
+Vertex createsVertex(int);
+int addVertex(Vertex*, int);
+int removeVertex(Vertex*, int);
+Vertex predecessor(Vertex*);
+Vertex successor(Vertex*);
+int search(Vertex *, int);
+void preorder(Vertex);
+void inorder(Vertex);
+void postorder(Vertex);
+void displayNested(Vertex);
+void displayIndented(Vertex, int);
+void buildTree(Vertex *, char *);
+void charcat(char *, char);
+void clearTree(Vertex *);
+int rotateVertex(Vertex *, int, int);
+int leftRotation(Vertex *);
+int rightRotation(Vertex *);
 
-int main() {
+int main()
+{
+   Vertex root = NULL;
 
-    ArvBinPesq Raiz = NULL;
+   // char *tree = "50(30(20(10),40), 70(60,80(,90)))";
+   char* tree = "4(2(1,3),6(5,7))";
 
-    char *arvore = "50(30(20(10),40), 70(60,80(,90)))";
+   printf("========================Binary Search Tree - BST========================\n");
 
-    printf("====================Arvore Binaria de Pesquisa - ABP====================\n");
+   buildTree(&root, tree);
 
-    constroiArvore(&Raiz, arvore);
-    /*insereVertice(&Raiz, 51);
-    insereVertice(&Raiz, 52);
-    insereVertice(&Raiz, 53);*/
-    //destroiArvore(&Raiz);
-    printf("\nRepresentacao aninhada: ");
-    representacaoAninhada(Raiz);
+   //removeVertex(&root, 2);
+   //removeVertex(&root, 1);
 
-    printf("\nRepresentacao indentada: \n");
-    representacaoIndentada(Raiz, 0);
-    printf("\n");
+   printf("\nNested representation: ");
+   displayNested(root);
 
-    printf("Percursos:");
-    printf("\n  PreOrdem: ");
-    preOrdem(Raiz);
-    printf("\n  InOrdem: ");
-    emOrdem(Raiz);
-    printf("\n  PosOrdem: ");
-    posOrdem(Raiz);
-    printf("\n");
+   printf("\nIndented representation: \n");
+   displayIndented(root, 0);
+   printf("\n");
 
-    /*giraVertice(&Raiz, 51, 1);
-    representacaoAninhada(Raiz);
-    printf("\n");
-    insereVertice(&Raiz, 55);
-    representacaoAninhada(Raiz);
-    printf("\n");*/
+   printf("Traversals:");
+   printf("\n  Preorder: ");
+   preorder(root);
+   printf("\n  Inorder: ");
+   inorder(root);
+   printf("\n  Postorder: ");
+   postorder(root);
+   printf("\n");
 
-    printf("========================================================================\n\n");
+   clearTree(&root);
+   
+   printf("========================================================================\n\n");
 
-    /*removeVertice(&Raiz, 4);
-    removeVertice(&Raiz, 6);
-    removeVertice(&Raiz, 5);
-    removeVertice(&Raiz, 3);
-    removeVertice(&Raiz, 2);
-    removeVertice(&Raiz, 1);
-    removeVertice(&Raiz, 7);
-    insereVertice(&Raiz, 53);
-    insereVertice(&Raiz, 21);
-    insereVertice(&Raiz, 79);*/
 
-    system("pause");
-    return 0;
+   system("pause");
+   return EXIT_SUCCESS;
 }
 
-int arvoreVazia(ArvBinPesq* ptrRaiz) {
-    return *ptrRaiz == NULL;
+
+int isEmpty(Vertex root) {
+   return root == NULL;
 }
 
-ArvBinPesq criaVertice(int x) {
-    ArvBinPesq novoVertice;
-    if(novoVertice = (ArvBinPesq)malloc(sizeof(Vertice))) {
-        novoVertice->esq = NULL;
-        novoVertice->chave = x;
-        novoVertice->dir = NULL;
-        return novoVertice;
-    }
-    return NULL;
+
+Vertex createsVertex(int key) {
+   Vertex newVertex = NULL;
+   if (newVertex = (Vertex)malloc(sizeof(struct vertex))) {
+      newVertex->left = NULL;
+      newVertex->key = key;
+      newVertex->right = NULL;
+   }
+   return newVertex;
 }
 
-int insereVertice(ArvBinPesq* ptrRaiz, int x) { // [ptrRaiz]----->[Raiz]----->...
-    ArvBinPesq aux;                             //                [aux]----->
-    aux = *ptrRaiz;                             //                [aux]----->...
-    if (arvoreVazia(&aux)) {                    //     se         [aux]----->\ .
-        aux = criaVertice(x);                   //                [aux]----->[\|x|\]
-        *ptrRaiz = aux;                         // [ptrRaiz]----->[Raiz]---->[\|x|\]
-        return 1;
-    } else {
-        if (x < aux->chave)
-            return insereVertice(&(aux->esq), x);
-        else {
-            if (x == aux->chave)                // chave já existente n�o pode inserir
-                return 0;
-            else    // (x > aux->chave)
-                return insereVertice(&(aux->dir), x);
-        }
-    }
+int addVertex(Vertex* root_ptr, int data) {
+
+   Vertex root = *root_ptr;
+
+   if (isEmpty(root)) {
+      root = createsVertex(data);
+      *root_ptr = root;
+      return 1;
+   } else {
+      if (data < root->key) {
+         return addVertex(&root->left, data);
+      } else {
+         if (data == root->key) {
+            return 0;
+         } else {
+            return addVertex(&root->right, data);
+         }
+      }
+   }
 }
 
-int removeVertice(ArvBinPesq* ptrRaiz, int x){
-    ArvBinPesq aux; //aux: vértice que será removido
 
-    if (*ptrRaiz == NULL)   // árvore nula ou elemento a ser removido não está na árvore
-        return 0;
+int removeVertex(Vertex* root_ptr, int data) {
+   Vertex root; //root: vértice que será removido
 
-    if (x == (*ptrRaiz)->chave) {  // vértice encontrado! A remover ...
-        aux = *ptrRaiz; // aux aponta para a Raiz atual, que é o vértice a ser removido
-        if ((*ptrRaiz)->dir == NULL) {  // vértice de grau 0, ou de grau 1, com sub-arvore esquerda
-            *ptrRaiz = (*ptrRaiz)->esq; // Raiz atual (vértice que será removido) passa a apontar para sua única sub-árvore, neste caso, a esquerda. No caso de grau 0, a sub-árvore esquerda também será nula, o que fará com que a árvore final seja nula (vazia)
-        } else {
-            if ( (*ptrRaiz)->esq == NULL)   // vértice de grau 1, com sub-árvore direita
-                *ptrRaiz = (*ptrRaiz)->dir; // Raiz atual (vértice que será removido) passa a apontar para sua única sub-árvore, neste caso, a direita
-            else {  /* remoção através da troca de posição entre o elemento a ser eliminado e seu antecessor */
-                aux = antecessorVertice(&(aux->esq));   // "isola" o antecessor para ser eliminado
-                (*ptrRaiz)->chave = aux->chave; // coloca a chave do antecessor na raiz da árvore ou sub-árvore
-            }
-        }
-        free(aux);  // libera a memória
-        return 1;
-    } else {
-        if (x < (*ptrRaiz)->chave) // vértice a ser removido é menor do que a raiz da árvore ou da subárvore
-            return removeVertice(&((*ptrRaiz)->esq), x);
-        else // (aux->chave < x) vértice a ser removido é maior do que a raiz da árvore ou da subárvore
-            return removeVertice(&((*ptrRaiz)->dir), x);
-    }
+   if (*root_ptr == NULL) // árvore nula ou elemento a ser removido não está na árvore
+      return 0;
+
+   if (data == (*root_ptr)->key) { // vértice encontrado! A remover ...
+      root = *root_ptr; // root aponta para a Raiz atual, que é o vértice a ser removido
+      if ((*root_ptr)->right == NULL) { // vértice de grau 0, ou de grau 1, com sub-arvore esquerda
+         *root_ptr = (*root_ptr)->left; // Raiz atual (vértice que será removido) passa a apontar para sua única sub-árvore, neste caso, a esquerda. No caso de grau 0, a sub-árvore esquerda também será nula, o que fará com que a árvore final seja nula (vazia)
+      }
+      else {
+         if ((*root_ptr)->left == NULL)   // vértice de grau 1, com sub-árvore direita
+            *root_ptr = (*root_ptr)->right; // Raiz atual (vértice que será removido) passa a apontar para sua única sub-árvore, neste caso, a direita
+         else { //remoção através da troca de posição entre o elemento a ser eliminado e seu antecessor
+            root = predecessor(&(root->left)); // "isola" o antecessor para ser eliminado
+            (*root_ptr)->key = root->key;       // coloca a chave do antecessor na raiz da árvore ou sub-árvore
+         }
+      }
+      free(root); // libera a memória
+      return 1;
+   }
+   else
+   {
+      if (data < (*root_ptr)->key) // vértice a ser removido é menor do que a raiz da árvore ou da subárvore
+         return removeVertex(&((*root_ptr)->left), data);
+      else // (root->key < x) vértice a ser removido é maior do que a raiz da árvore ou da subárvore
+         return removeVertex(&((*root_ptr)->right), data);
+   }
 }
 
-ArvBinPesq antecessorVertice(ArvBinPesq* ptrRaiz) {
-    ArvBinPesq aux = *ptrRaiz;
-    if (!aux->dir) { // se aux->dir é nulo, então aux é o antecessor
-        *ptrRaiz = (*ptrRaiz)->esq;
-        return aux;
-    } else
-        return antecessorVertice(&(aux->dir));
+
+Vertex predecessor(Vertex *root_ptr) {
+   Vertex root = *root_ptr;
+   if (!root->right) { // se root->right é nulo, então root é o antecessor
+      *root_ptr = (*root_ptr)->left;
+      return root;
+   }
+   else
+      return predecessor(&(root->right));
 }
 
-ArvBinPesq sucessorVertice(ArvBinPesq* ptrRaiz) {
-    ArvBinPesq aux = *ptrRaiz;
-    if (!aux->esq) {
-        *ptrRaiz = (*ptrRaiz)->dir;
-        return aux;
-    } else
-        return sucessorVertice(&(aux->esq));
+
+Vertex successor(Vertex *root_ptr) {
+   Vertex root = *root_ptr;
+   if (!root->left) {
+      *root_ptr = (*root_ptr)->right;
+      return root;
+   } else
+      return successor(&(root->left));
 }
 
-int buscaVertice(ArvBinPesq* ptrRaiz, int x) {
-    ArvBinPesq aux = *ptrRaiz;
-    if (aux) {
-        if (x < aux->chave)
-            return buscaVertice(&aux->esq, x);
-        else {
-            if (x == aux->chave)
-                return 1;
-            else
-                return buscaVertice(&aux->dir, x);
-        }
-    }
-    return 0;
+
+int search(Vertex *root_ptr, int data) {
+   Vertex root = *root_ptr;
+   if (root) {
+      if (data < root->key)
+         return search(&root->left, data);
+      else {
+         if (data == root->key)
+            return 1;
+         else
+            return search(&root->right, data);
+      }
+   }
+   return 0;
 }
 
-void preOrdem(ArvBinPesq ptrRaiz) {
-    ArvBinPesq aux = ptrRaiz;
-    if (aux) {
-        printf("%d  ", aux->chave);
-        preOrdem(aux->esq);
-        preOrdem(aux->dir);
-    }
+
+void preorder(Vertex root_ptr) {
+   Vertex root = root_ptr;
+   if (root) {
+      printf("%d  ", root->key);
+      preorder(root->left);
+      preorder(root->right);
+   }
 }
 
-void emOrdem(ArvBinPesq ptrRaiz) {
-    ArvBinPesq aux = ptrRaiz;
-    if (aux) {
-        emOrdem(aux->esq);
-        printf("%d  ", aux->chave);
-        emOrdem(aux->dir);
-    }
+
+void inorder(Vertex root_ptr) {
+   Vertex root = root_ptr;
+   if (root) {
+      inorder(root->left);
+      printf("%d  ", root->key);
+      inorder(root->right);
+   }
 }
 
-void posOrdem(ArvBinPesq ptrRaiz) {
-    ArvBinPesq aux = ptrRaiz;
-    if (aux) {
-        posOrdem(aux->esq);
-        posOrdem(aux->dir);
-        printf("%d  ", aux->chave);
-    }
+
+void postorder(Vertex root_ptr) {
+   Vertex root = root_ptr;
+   if (root) {
+      postorder(root->left);
+      postorder(root->right);
+      printf("%d  ", root->key);
+   }
 }
 
-void representacaoAninhada(ArvBinPesq ptrRaiz) {
-    ArvBinPesq aux = ptrRaiz;
-    if (aux) {
-        printf("%d", aux->chave);
-        if (aux->esq || aux->dir) {
-            printf("(");
-            representacaoAninhada(aux->esq);
-            if (aux->dir){
-                printf(",");
-                representacaoAninhada(aux->dir);
-            }
-            printf(")");
-        }
-    }
+
+void displayNested(Vertex root_ptr) {
+   Vertex root = root_ptr;
+   if (root) {
+      printf("%d", root->key);
+      if (root->left || root->right) {
+         printf("(");
+         displayNested(root->left);
+         if (root->right) {
+            printf(",");
+            displayNested(root->right);
+         }
+         printf(")");
+      }
+   }
 }
 
-void representacaoIndentada(ArvBinPesq ptrRaiz, int tabs) {
-    ArvBinPesq aux = ptrRaiz;
-    int i;
-    if (aux == NULL)
-        return;
-    for(i = 0; i < tabs; i++)
-        printf("  ");
-    printf("%d\n", aux->chave);
-    representacaoIndentada(aux->esq, tabs+1);
-    representacaoIndentada(aux->dir, tabs+1);
+
+void displayIndented(Vertex root_ptr, int tabs) {
+   Vertex root = root_ptr;
+   int i;
+   if (root == NULL)
+      return;
+   for (i = 0; i < tabs; i++)
+      printf("  ");
+   printf("%d\n", root->key);
+   displayIndented(root->left, tabs + 1);
+   displayIndented(root->right, tabs + 1);
 }
 
-void constroiArvore(ArvBinPesq* ptrRaiz, char* arvore) {
-    int i, j, x;
-    int tam = strlen(arvore);
-    char aux[5] = "";
-    for(i = 0; i < tam; i++) {
-        strcpy(aux, "");
-        while ( (((arvore[i] != '(' && arvore[i] != ')') && arvore[i] != ',') && arvore[i] != ' ') && arvore[i] != '\0'){
-            concatenarCaractere(aux, arvore[i]);
-            i++;
-        }
-        if (strlen(aux) > 0) {
-            x = atoi(aux);
-            insereVertice(ptrRaiz, x);
-        }
-    }
+void buildTree(Vertex *root_ptr, char *tree) {
+   int i, j, data;
+   int size = strlen(tree);
+   char aux[5] = "";
+   for (i = 0; i < size; i++) {
+      strcpy(aux, "");
+      while ((((tree[i] != '(' && tree[i] != ')') && tree[i] != ',') && tree[i] != ' ') && tree[i] != '\0') {
+         charcat(aux, tree[i]);
+         i++;
+      }
+      if (strlen(aux) > 0) {
+         data = atoi(aux);
+         addVertex(root_ptr, data);
+      }
+   }
 }
 
-void concatenarCaractere(char* palavra, char caractere) {
-    int tam = strlen(palavra);
-    palavra[tam] = caractere;
-    palavra[tam+1] = '\0';
+
+void charcat(char *palavra, char caractere)
+{
+   int tam = strlen(palavra);
+   palavra[tam] = caractere;
+   palavra[tam + 1] = '\0';
 }
 
-void destroiArvore(ArvBinPesq* ptrRaiz) {
-    ArvBinPesq aux = *ptrRaiz;
-    if (aux != NULL) {
-        destroiArvore(&aux->esq);
-        destroiArvore(&aux->dir);
-        free(aux);
-        *ptrRaiz = NULL;
-    }
+
+void clearTree(Vertex *root_ptr) {
+   Vertex root = *root_ptr;
+   if (root != NULL) {
+      clearTree(&root->left);
+      clearTree(&root->right);
+      free(root);
+      *root_ptr = NULL;
+   }
 }
 
-int giraVertice(ArvBinPesq* ptrRaiz, int x, int sent) {
-    if (*ptrRaiz != NULL) {
-        if ((*ptrRaiz)->chave == x) {   // vertice a ser rotacionado foi encontrado
-            if (sent > 0)
-                rotacaoEsquerda(ptrRaiz);
-            else
-                rotacaoDireita(ptrRaiz);
-        } else {
-            if (x < (*ptrRaiz)->chave)
-                giraVertice(&(*ptrRaiz)->esq, x, sent);
-            else
-                giraVertice(&(*ptrRaiz)->dir, x, sent);
-        }
-    } else
-        return 0;
+
+int rotateVertex(Vertex *root_ptr, int data, int direction) {
+   if (*root_ptr != NULL) {
+      if ((*root_ptr)->key == data) { // vertice a ser rotacionado foi encontrado
+         if (direction > 0)
+            leftRotation(root_ptr);
+         else
+            rightRotation(root_ptr);
+      } else {
+         if (data < (*root_ptr)->key)
+            rotateVertex(&(*root_ptr)->left, data, direction);
+         else
+            rotateVertex(&(*root_ptr)->right, data, direction);
+      }
+   } else
+      return 0;
 }
 
-int rotacaoEsquerda(ArvBinPesq* V) {
-    ArvBinPesq aux; // elemento que tomara o lugar de V, quando V rotacionar
-    if (*V && (*V)->dir) {  // se existe um elemento para rotacionar e esse elemento possui filho à direita
-        aux = (*V)->dir;    // guardando referencia
-        (*V)->dir = aux->esq;   // o filho direito de V passa a ser o filho esquerdo de aux
-        aux->esq = *V;
-        (*V) = aux;
-        return 1;
-    }
-    return 0;
+
+int leftRotation(Vertex *V) {
+   Vertex root; // elemento que tomara o lugar de V, quando V rotacionar
+   if (*V && (*V)->right) {                        // se existe um elemento para rotacionar e esse elemento possui filho à direita
+      root = (*V)->right;      // guardando referencia
+      (*V)->right = root->left; // o filho direito de V passa a ser o filho esquerdo de root
+      root->left = *V;
+      (*V) = root;
+      return 1;
+   }
+   return 0;
 }
 
-int rotacaoDireita(ArvBinPesq* V) {
-    ArvBinPesq aux;
-    if (*V && (*V)->esq) {
-        aux = (*V)->esq;
-        (*V)->esq = aux->dir;
-        aux->dir = *V;
-        (*V) = aux;
-        return 1;
-    }
-    return 0;
+
+int rightRotation(Vertex *V) {
+   Vertex root;
+   if (*V && (*V)->left) {
+      root = (*V)->left;
+      (*V)->left = root->right;
+      root->right = *V;
+      (*V) = root;
+      return 1;
+   }
+   return 0;
 }
